61b339f9fa8a60ebce3785241a1910fbab634184
TomMonaco27 <66220098+TomMonaco27@users.noreply.github.com>
2021-02-25 14:42:08 +0300

    Add files via upload

---
+++ JS_Lesson_3_Task_4.js
@@ -0,0 +1,15 @@
+// JS_Lesson_3_Task_4. Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+
+for (var i = 0; i < 10; console.log(i++)){
+}
+
+// Вариатн 2. C использованием функции
+
+function isNext(i){
+	console.log(i);
+	return i < 9
+}
+
+for (var i = 0; isNext(i); i++) {
+	
+}
